07e029e69364febf7383db8333279dea50fdc493
Yemi Akinyemi <yakineymi@baosystems.com>
2022-02-23 19:32:24 +0000

    feat: amplify init

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "projecta": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
